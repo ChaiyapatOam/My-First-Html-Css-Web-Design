d4aa0166eb9c04dfdee9395875ecb13c0506de02
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2021-11-20 16:51:11 +0700

    add nodejs express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const router = express.Router()
+const path = require('path')
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname });
+});
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "style.css");
+  });
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/',router)
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
